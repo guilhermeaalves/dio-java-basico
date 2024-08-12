d3a588a305e8dd195ec57ffee21779cfa8ed95e5
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-08-12 16:59:05 -0300

    feat: estudando tipos de variaveis

--- src/edu/guilherme/segundasemana/TiposVariaveis.java
+++ src/edu/guilherme/segundasemana/TiposVariaveis.java
@@ -1,5 +1,7 @@
 package edu.guilherme.segundasemana;
 
+import java.util.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) {
 
@@ -10,8 +12,11 @@ public class TiposVariaveis {
        int cep = 21070333; // se começar com 0, talvez tenha que ser outro tipo
        long cpf = 98765432109L; // se começar com 0, talvez tenha que ser outro tipo
        float pi = 3.14F;
-       double salario = 1300.33;
+       double salario = 1300.33; 
        String meuNome = "Guilherme Alves"; 
+       char sexo = 'M'; // usado para apenas 1 caracter 
+       boolean estaTrabalhando = true; // usado para valores booleanos, como true ou false
+       Date dataNascimento = new Date(); // criando um novo objeto
 
         // tipo long precisa terminar com L e tipo float com F
 
@@ -20,5 +25,7 @@ public class TiposVariaveis {
         short numeroCurto2 = (short) numeroNormal;
 
         final int NUMERO = 1000; // assim que é declarada uma const no Java, com a palavra final e deve ser escrita em CAIXA ALTA.
+
+        
     }
 }
