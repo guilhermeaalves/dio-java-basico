15405a9fdbe50d51af32b6a9f3e74e73b1c8f24e
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-08-14 09:26:49 -0300

    feat: utilizando scanner

---
+++ src/edu/guilherme/segundasemana/AboutMe.java
@@ -0,0 +1,32 @@
+package edu.guilherme.segundasemana;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        
+        
+    }
+}
+
